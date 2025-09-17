be23274866914d98be757c4d2b8c542248fa2605
Natalia Miranda <natumiranda.28@gmail.com>
2025-09-17 20:47:45 -0300

    Delete laboratorioadicional2.py

--- laboratorioadicional2.py
+++
@@ -1,8 +0,0 @@
-""" Ejercicio 1: Mostrar y eliminar
-Utilizando un bucle “while”, elaborar un código que imprima en pantalla cada uno de los elementos de una lista y simultáneamente los elimine, hasta quedar vacía """
-
-lista_elementos = ['manzana', 'banana', 'cereza', 'durazno']
-while lista_elementos:  
-    elemento = lista_elementos.pop(0)  
-    print("Elemento eliminado:", elemento)  
-print("Lista final:", lista_elementos)  
